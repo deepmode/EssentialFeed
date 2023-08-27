427c25b9fa02330b35d79bb9df63606f922538d2
Eric Ho <eric.ho@101medialab.com>
2023-08-27 08:56:36 +0800

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Eric Ho on 8/27/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id:UUID
+    let description:String?
+    let location:String?
+    let image:String
+}
