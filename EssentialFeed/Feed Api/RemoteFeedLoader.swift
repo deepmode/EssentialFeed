db9c19310b6041ece70786836409553fcd5d55c7
Eric Ho <eric.ho@101medialab.com>
2023-09-02 08:58:56 +0800

    Delivers items array on 200 HTTP response with JSON items

--- EssentialFeed/Feed Api/RemoteFeedLoader.swift
+++ EssentialFeed/Feed Api/RemoteFeedLoader.swift
@@ -43,8 +43,8 @@ public final class RemoteFeedLoader {
             print("\(type(of:self)): \(#function)) result")
             switch result {
             case .success(let data, _ ):
-                if let _ = try? JSONSerialization.jsonObject(with: data) {
-                    completion(.success([]))
+                if let root = try? JSONDecoder().decode(Root.self, from: data) {
+                    completion(.success(root.items))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -55,4 +55,7 @@ public final class RemoteFeedLoader {
     }
 }
 
+private struct Root: Decodable {
+    let items:[FeedItem]
+}
 
