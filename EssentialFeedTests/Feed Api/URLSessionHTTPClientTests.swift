53cc518e1c5ad13585cc6e6e9e5babc104e9c025
Eric Ho <eric.ho@101medialab.com>
2023-09-08 08:56:54 +0800

    Make URLProtocolStub intercept every network request instead of URL-based mocking since we don't want to ever perform a network request during tests. Also we want our assertion to be more precise about the error when they fail.

--- EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
@@ -33,7 +33,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let url = URL(string: "https://any-url.com/")!
         let error = NSError(domain: "any error", code: 1)
     
-        URLProtocolStub.stubs(url:url, data: nil, response: nil, error:error)
+        URLProtocolStub.stub(data: nil, response: nil, error:error)
         
         let sut = URLSessionHTTPClient()
     
@@ -59,7 +59,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
     
     private class URLProtocolStub: URLProtocol {
         
-        private static var stubs = [URL:Stub]()
+        private static var stub:Stub?
         
         private struct Stub {
             let data:Data?
@@ -67,8 +67,8 @@ final class URLSessionHTTPClientTests: XCTestCase {
             let error:Error?
         }
         
-        static func stubs(url:URL, data: Data?, response:HTTPURLResponse?, error:Error?) {
-            stubs[url] = Stub(data: data, response: response, error: error)
+        static func stub(data: Data?, response:HTTPURLResponse?, error:Error?) {
+            stub = Stub(data: data, response: response, error: error)
         }
         
         static func startInterceptingRequet() {
@@ -77,13 +77,11 @@ final class URLSessionHTTPClientTests: XCTestCase {
         
         static func stopInterceptingRequet() {
             URLProtocolStub.unregisterClass(URLProtocolStub.self)
-            stubs = [:]
+            stub = nil
         }
         
         override class func canInit(with request: URLRequest) -> Bool {
-            guard let url = request.url else { return false }
-            
-            return stubs[url] != nil
+            return true
         }
         
         override class func canonicalRequest(for request: URLRequest) -> URLRequest {
@@ -91,17 +89,16 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
         
         override func startLoading() {
-            guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { return }
-            
-            if let data = stub.data {
+
+            if let data = URLProtocolStub.stub?.data {
                 client?.urlProtocol(self, didLoad: data)
             }
             
-            if let response = stub.response {
+            if let response = URLProtocolStub.stub?.response {
                 client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
             }
             
-            if let error = stub.error {
+            if let error = URLProtocolStub.stub?.error {
                 client?.urlProtocol(self, didFailWithError: error)
             }
             
@@ -114,36 +111,4 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
 
     }
-    
-    
-
-//    override func setUpWithError() throws {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//
-//        // In UI tests it is usually best to stop immediately when a failure occurs.
-//        continueAfterFailure = false
-//
-//        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-//    }
-//
-//    override func tearDownWithError() throws {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
-//
-//    func testExample() throws {
-//        // UI tests must launch the application that they test.
-//        let app = XCUIApplication()
-//        app.launch()
-//
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
-//
-//    func testLaunchPerformance() throws {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTApplicationLaunchMetric()]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
 }
