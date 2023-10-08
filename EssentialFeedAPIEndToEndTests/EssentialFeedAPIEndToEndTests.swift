c50e442378e6b27fedeb5457e5b561822e80aafc
Eric Ho <eric.ho@101medialab.com>
2023-10-08 12:06:51 +0800

    rename the test func

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -10,7 +10,7 @@ import EssentialFeed
 
 final class EssentialFeedAPIEndToEndTests: XCTestCase {
 
-    func test_endToEnd() {
+    func test_endToEndTestServerGetFeedResult_matchesFixedTestAccountData() {
         
         let url = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
